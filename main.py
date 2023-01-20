20890aef6164feef6b28da93c2066927ada54a83
Hikaru Satomura <saosystem0@gmail.com>
2023-01-21 08:51:34 +0900

    平均律でWaveファイルを作成

--- main.py
+++ main.py
@@ -12,11 +12,15 @@ def CreateWaveStereo( leftHz : float, rightHz : float, waveRange : int, filename
     N  = WaveDefine.WaveData.Framerate * s     #フレーム数（左右で1フレーム）
     t = np.arange(0, s, 1 / WaveDefine.WaveData.Framerate)   #横軸
 
+    r12 = 2 ** (1/12) #2の12乗根（≒1.059463094）
+
     #sin(2π*□＊t) の周波数は□の部分。16ビットで録音するので、
-    #－32768～32767がデータの範囲。だから－15000～15000の範囲の
-    #大きさの音を作ることにした。
-    leftData = waveRange * np.sin(2*pi*leftHz*t)
-    rightData = waveRange * np.sin(2*pi*rightHz*t)
+    #－32768～32767がデータの範囲。
+    
+    #平均律
+    leftData = waveRange * np.sin(2*pi*leftHz*t) + waveRange * np.sin(2*pi*leftHz*(r12**4)*t)
+    rightData = waveRange * np.sin(2*pi*rightHz*(r12**7)*t)
+
     soundData = np.zeros(N * 2, dtype= "int16") #左右のデータ分が必要
     soundData[0::2] = leftData # 0番目から2要素ずつ代入
     soundData[1::2] = rightData # 1番目から2要素ずつ代入
@@ -35,7 +39,7 @@ print('▼▼▼▼ {}:{}:{}'.format(dt.hour, dt.minute, dt.second))
 
 # L:ラ R:ド　の音声ファイルを作成
 # #Hzは振幅の回数　100Hzの場合0~10000~0~-10000~0　という変化を1秒間に100回繰り返す
-CreateWaveStereo(WaveDefine.MusicalScale.Ra, WaveDefine.MusicalScale.So, 10000, "sample.wav")
+CreateWaveStereo(WaveDefine.MusicalScale.Do, WaveDefine.MusicalScale.Do, 10000, "do_heikinn.wav")
 
 dt = datetime.datetime.now()
 print('▲▲▲▲ {}:{}:{}'.format(dt.hour, dt.minute, dt.second))
